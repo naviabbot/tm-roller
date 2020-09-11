cf9c478dba110e57aaebf86929590c86863d63d6
Joshua Cain <joshuacain@joshcain.ga>
2020-09-10 20:26:04 -0500

    Reworked roller to match official parameters. Old roller is now in 'regression-of-gods' branch.

--- tm-roller.dart
+++ tm-roller.dart
@@ -7,49 +7,46 @@ import 'dart:math';
 String quality (int roll) {
   /// This function takes in a roll from MAIN and compares it to a class.
   String qual;
-  if (roll == 1) {
-    qual = "EX-Class";
-  } else if (roll > 1 && roll <= 5) {
-    qual = "A-Class";
-  } else if (roll > 5 && roll <= 10) {
-    qual = "B-Class";
-  } else if (roll > 10 && roll <= 15) {
-    qual = "C-Class";
+  if (roll >=1 && roll <= 4) {
+    qual = "A-Rank (50)";
+  } else if (roll > 4 && roll <= 8) {
+    qual = "B-Rank (40)";
+  } else if (roll > 9 && roll <= 12) {
+    qual = "C-Rank (30)";
+  } else if (roll > 12 && roll <= 14){
+    qual = "D-Rank (20)";
   } else {
-    qual = "D-Class";
+    qual = "E-Rank (10)";
   }
   return qual;
 }
 
 quantity (String quality) {
-  /// This function takes in a class from Function 'quality' and does dice magic to roll the number of circuits
-  var die2 = Random();
-  int pwr;
-  switch (quality){
-    case 'EX-Class':
-      pwr = die2.nextInt(100) + die2.nextInt(100) + die2.nextInt(100) + 103;
-      return pwr;
-    case 'A-Class':
-      pwr = die2.nextInt(100) + die2.nextInt(100) + 102;
-      return pwr;
-    case 'B-Class':
-      pwr = die2.nextInt(100) + 1;
-      return pwr;
-    case 'C-Class':
-      pwr = die2.nextInt(75) + 1;
-      return pwr;
-    case 'D-Class':
-      pwr = die2.nextInt(50) + 1;
-      return pwr;
+  /// This function takes in a class from Function 'quality' and does dice magic to roll the number of circuits.
+  String mod;
+  var roll2 = Random();
+  int pwr = roll2.nextInt(4) + 1;
+  switch (pwr){
+    case 1:
+      mod = "+";
+      return mod;
+    case 2:
+      mod = "++";
+      return mod;
+    case 3:
+      mod = "+++";
+      return mod;
     default:
-      return 'Data Not Found';
+      mod = "(No Modifier)";
+      return mod;
   }
 }
 
 void main () {
+  print("Type-Moon Circuit Roller. Modifiers multiply the default number by the following - A: 5, B: 4, C: 3, D: 2, E: 1.");
   var die1 = Random();
   var roll = die1.nextInt(19) + 1;
   String clazz = quality(roll);
   var numbuhz = quantity(clazz);
-  print("There are $numbuhz $clazz circuits.");
+  print("$clazz $numbuhz circuits.");
 }
